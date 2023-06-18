c2777b663ceead8781e2f3e876b4b4133fe80326
kirtymeena <kirtymeena@gmail.com>
2023-06-18 12:15:36 +0530

    Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
     mode: "development",
     target:'web',
@@ -54,6 +54,5 @@ module.exports = {
             filename: "index.html",
             template: 'src/index.html'
         }),
-        // new BundleAnalyzerPlugin(),
     ]
 }
